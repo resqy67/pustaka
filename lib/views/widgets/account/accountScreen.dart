c8a2cbc4228e5ff460b7c9e614ae934919f21a23
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-05-21 13:03:57 +0800

    chore: Update AccountScreen UI with user profile information and navigation options
    The AccountScreen UI has been updated to display the user's profile information, including their profile picture, name, and reading progress. Additionally, navigation options for managing the account and logging out have been added. These changes aim to improve the user experience and provide better functionality for managing the user's account.

--- lib/views/widgets/account/accountScreen.dart
+++ lib/views/widgets/account/accountScreen.dart
@@ -4,12 +4,96 @@ class AccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Account'),
-      ),
-      body: Center(
-        child: Text('Account Screen'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(
+            'User!',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(20),
+                    child: CircleAvatar(
+                      radius: 50,
+                      backgroundImage:
+                          NetworkImage('https://picsum.photos/200/320'),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 25),
+                    width: MediaQuery.of(context).size.width * 0.6,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          '35 / 60 Buku dibaca',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Card(
+              child: ListTile(
+                title: Text('Account'),
+                subtitle: Text('Manage your account'),
+                leading: Icon(Icons.account_circle),
+                onTap: () {
+                  Navigator.pushNamed(context, '/account');
+                },
+              ),
+            ),
+            SizedBox(height: 5),
+            // Card(
+            //   child: ListTile(
+            //     title: Text('Settings'),
+            //     subtitle: Text('Manage your settings'),
+            //     leading: Icon(Icons.settings),
+            //     onTap: () {
+            //       Navigator.pushNamed(context, '/settings');
+            //     },
+            //   ),
+            // ),
+            Card(
+              child: ListTile(
+                title: Text('Logout'),
+                subtitle: Text('Logout from your account'),
+                leading: Icon(Icons.logout),
+                onTap: () {
+                  Navigator.pushNamed(context, '/logout');
+                },
+              ),
+            ),
+          ],
+        )));
   }
 }
