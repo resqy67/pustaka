bea8655d6b34b366fd0ebc79934399af7df6a998
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-05-27 00:16:10 +0800

    chore: Add AccountDetail widget for displaying user account details
    The AccountDetail widget has been added to display the user's account details, including their profile picture, name, description, class, and password. This change aims to enhance the AccountScreen UI and provide a better user experience for managing the user's account.

---
+++ lib/views/widgets/account/accountDetail.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+class AccountDetail extends StatelessWidget {
+  const AccountDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Detail Akun!',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                child: CircleAvatar(
+                  radius: 100,
+                  backgroundImage:
+                      NetworkImage('https://picsum.photos/200/320'),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.person),
+                    SizedBox(width: 10),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Nama Lengkap',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text('lorem ipsum doldolore magna aliqua'),
+                        ],
+                      ),
+                    ),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.circle),
+                    SizedBox(width: 10),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Deskripsi',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text('lorem ipsum doldolore magna aliqua'),
+                        ],
+                      ),
+                    ),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.class_rounded),
+                    // SizedBox(width: 10),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Kelas',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text('12 RPL 1'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    SizedBox(width: 50),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.password_rounded),
+                    // SizedBox(width: 10),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Password',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text('*********'),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    SizedBox(width: 50),
+                    Icon(Icons.edit),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
