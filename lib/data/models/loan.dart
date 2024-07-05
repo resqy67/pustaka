1d544113a84ae68bcb8a56caac6f6d7f29af0679
Resqy Hedy Kurniawan <resqyhedy@gmail.com>
2024-07-05 17:13:35 +0800

    chore: Update Loan model with bookUuid field

--- lib/data/models/loan.dart
+++ lib/data/models/loan.dart
@@ -1,39 +1,43 @@
 class Loan {
-  final String userId;
+  // final String userId;
   final String loanDate;
   final String returnDate;
   final String bookTitle;
   final String bookImage;
   final String bookAuthor;
+  final String bookUuid;
 
   Loan({
-    required this.userId,
+    // required this.userId,
     required this.loanDate,
     required this.returnDate,
     required this.bookTitle,
     required this.bookImage,
     required this.bookAuthor,
+    required this.bookUuid,
   });
 
   factory Loan.fromJson(Map<String, dynamic> json) {
     return Loan(
-      userId: json['user_id'].toString(),
+      // userId: json['user_id'].toString(),
       loanDate: json['loan_date'].toString(),
       returnDate: json['return_date'].toString(),
       bookTitle: json['book']['title'].toString(),
       bookImage: json['book']['image'].toString(),
       bookAuthor: json['book']['author'].toString(),
+      bookUuid: json['book']['uuid'].toString(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'user_id': userId,
+      // 'user_id': userId,
       'loan_date': loanDate,
       'return_date': returnDate,
       'book_title': bookTitle,
       'book_image': bookImage,
       'book_author': bookAuthor,
+      'book_uuid': bookUuid,
     };
   }
 }
--- lib/views/components/book/loan/index.dart
+++ lib/views/components/book/loan/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pustaka/data/models/loan.dart';
+import 'package:pustaka/views/components/book/index.dart';
 
 Widget loanBooks(BuildContext context, LoanList? loanList) {
   return Padding(
@@ -22,77 +23,89 @@ Widget loanBooks(BuildContext context, LoanList? loanList) {
               return Container(
                 width: MediaQuery.of(context).size.width / 2 - 15,
                 height: 300,
-                child: Card(
-                  color: Colors.white,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Card(
-                        elevation: 0,
-                        clipBehavior: Clip.antiAlias,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Image.network(
-                              loan.bookImage,
-                              // 'https://picsum.photos/200/320',
-                              fit: BoxFit.cover,
-                            ),
-                            Positioned(
-                              top: 12, // Atur jarak dari atas
-                              left: 8, // Atur jarak dari kiri
-                              child: Container(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 8, vertical: 4),
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                child: Text(
-                                  '$days Hari',
-                                  // '7 Hari',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => BookPage(
+                          bookUuid: loan.bookUuid,
+                        ),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    color: Colors.white,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              Image.network(
+                                loan.bookImage,
+                                // 'https://picsum.photos/200/320',
+                                fit: BoxFit.cover,
+                              ),
+                              Positioned(
+                                top: 12, // Atur jarak dari atas
+                                left: 8, // Atur jarak dari kiri
+                                child: Container(
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 8, vertical: 4),
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Text(
+                                    '$days Hari',
+                                    // '7 Hari',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              loan.bookTitle,
-                              // 'Thinking, Fast and Slow',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                loan.bookTitle,
+                                // 'Thinking, Fast and Slow',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                                textAlign: TextAlign.left,
                               ),
-                              textAlign: TextAlign.left,
-                            ),
-                            Text(
-                              loan.bookAuthor,
-                              // 'James Clear',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w400,
+                              Text(
+                                loan.bookAuthor,
+                                // 'James Clear',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontFamily: 'Poppins',
+                                  fontWeight: FontWeight.w400,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               );
